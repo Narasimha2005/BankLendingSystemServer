cf0b15b9c319c17aff767cf6a3cc30cefeffb222
Narasimha2005 <148676298+Narasimha2005@users.noreply.github.com>
2025-07-25 23:52:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173','https://bank-lending-system-gamma.vercel.app/'],
-    credentials: true
-  }));
+app.use(cors());
 
 const dbPath = path.join(__dirname, 'bank.db');
 let db = null;
